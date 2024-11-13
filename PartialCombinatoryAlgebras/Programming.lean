76b7f973962e65eed9cfb592c0121de02e1e01df
Andrej Bauer <Andrej.Bauer@andrej.com>
2024-11-13 23:41:18 +0100

    Set up basic combinators for programming.

---
+++ PartialCombinatoryAlgebras/Programming.lean
@@ -0,0 +1,20 @@
+import PartialCombinatoryAlgebras.Basic
+
+namespace PCA
+
+  variable {A : Type*} [PCA A]
+
+  def I : Part A := sorry
+
+  def equal_I : Part A := sorry
+
+  def pair : Part A := sorry
+
+  def fst : Part A := sorry
+
+  def snd : Part A := sorry
+
+  def equal_pair : Part A := sorry
+
+
+  end PCA

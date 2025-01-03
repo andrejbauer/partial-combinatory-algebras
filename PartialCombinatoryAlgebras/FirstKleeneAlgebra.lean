9034b20a973d04da3ca81e2c30c1055f57fc83e4
Andrej Bauer <Andrej.Bauer@andrej.com>
2025-01-03 17:43:24 +0100

    wip: First Kleene algebra

--- PartialCombinatoryAlgebras/FirstKleeneAlgebra.lean
+++ PartialCombinatoryAlgebras/FirstKleeneAlgebra.lean
@@ -17,9 +17,9 @@ namespace FirstKleeneAlgebra
 open Nat.Partrec
 
 @[reducible]
-def kleeneApp : ℕ → ℕ →. ℕ := fun m n => Code.eval (Denumerable.ofNat Code m) n
+def app : ℕ → ℕ →. ℕ := Code.eval ∘ Denumerable.ofNat Code
 
-theorem kleeneApp_partrec₂ : Partrec₂ kleeneApp := by
+theorem app_partrec₂ : Partrec₂ app := by
   apply Partrec₂.comp₂ (f := Code.eval)
   · exact Code.eval_part
   · apply Computable.comp (f := Denumerable.ofNat Code) (g := Prod.fst)
@@ -28,32 +28,47 @@ theorem kleeneApp_partrec₂ : Partrec₂ kleeneApp := by
   · apply Computable.to₂
     apply Computable.snd
 
-instance hasDot: HasDot (Part ℕ) where
-  dot u v := u.bind (fun m => v.bind (fun n => kleeneApp m n))
+instance partialApplication: PartialApplication ℕ where
+  app u v := u.bind (fun m => v.bind (fun n => app m n))
 
-@[reducible]
-def K : ℕ → ℕ →. ℕ := Code.eval ∘ Code.const
-
-theorem K_part : Partrec₂ K := by
-  apply Partrec₂.comp₂ (f := Code.eval)
-  · exact Code.eval_part
-  · apply Computable.comp (f := Code.const) (g := Prod.fst)
+theorem K_part : Partrec (Part.some ∘ Code.encodeCode ∘ Code.const) := by
+  apply Computable.comp
+  · apply Computable.comp (f := Code.encodeCode)
+    · apply Computable.encode
     · exact Code.const_prim.to_comp
-    · apply Computable.fst
-  · apply Computable.snd
+  · exact Computable.id
 
-lemma eq_decode_encode (c : Code) : Denumerable.ofNat Code c.encodeCode = c := by
+lemma eq_ofNat_encodeCode (c : Code) : Denumerable.ofNat Code c.encodeCode = c := by
   rw [Code.ofNatCode_eq]
   induction c <;> simp [Code.encodeCode, Code.ofNatCode, Nat.div2_val, *]
 
-theorem K_code_exists : ∃ k : ℕ, ∀ (a b : ℕ), (Part.some k) ⬝ a ⬝ b = .some a := by
-  obtain ⟨c, H⟩ := Code.exists_code.mp K_part
-  rw [K] at H ; simp at H
+theorem K_exists : ∃ k : ℕ, ∀ (u v : Part ℕ), u ⇓ → v ⇓ → .some k ⬝ u ⬝ v = u := by
+  obtain ⟨c, evalc⟩ := Code.exists_code.mp K_part
   use c.encodeCode
-  intros a b
-  simp [hasDot, eq_decode_encode, kleeneApp]
-  rw [H]
+  rintro u v hu hv
+  simp [HasDot.dot, PartialApplication.app, app, eq_ofNat_encodeCode, evalc]
+  rw [← Part.some_get hu]
+  simp only [Part.bind_some]
+  rw [← Part.some_get hv]
+  simp only [Part.bind_some, eq_ofNat_encodeCode]
   simp
+
+theorem S_exists : ∃ s : ℕ, ∀ (u v w : Part ℕ), u ⇓ → v ⇓ → w ⇓ → .some s ⬝ u ⬝ v ⬝ w = (u ⬝ w) ⬝ (v ⬝ w) := by
+  sorry
+
+noncomputable instance isPCA : PCA ℕ where
+  K := .some K_exists.choose
+  S := .some S_exists.choose
+
+  df_K₀ := trivial
+  df_K₁ := sorry
+
+  df_S₀ := trivial
+  df_S₁ := sorry
+  df_S₂ := sorry
+
+  eq_K := K_exists.choose_spec
+  eq_S := S_exists.choose_spec
 
 
 end FirstKleeneAlgebra
